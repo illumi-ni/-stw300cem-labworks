92487083cb23977495c219cb9750ae569f7bfa0c
illumi-ni <krazyme53@gmail.com>
2020-12-19 20:33:16 +0545

    lab 1 question 1

--- app/src/main/java/com/ujjallamichhane/labworks/lab1/Lab1Question1Activity.kt
+++ app/src/main/java/com/ujjallamichhane/labworks/lab1/Lab1Question1Activity.kt
@@ -1,10 +1,10 @@
-package com.ujjallamichhane.labworks.Lab1
+package com.ujjallamichhane.labworks.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ujjallamichhane.labworks.R
 
-class Lab1_Question1_Activity : AppCompatActivity() {
+class Lab1Question1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lab1__question1_)
